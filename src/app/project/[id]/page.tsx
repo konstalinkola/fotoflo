68456456434702648102e71d39b578cc00ea1201
”Konsta <konsta.linkola@gmail.com>
2025-09-25 12:30:54 +0300

    Disable ESLint no-explicit-any for MVP build

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 
--- src/app/public/[projectId]/page.tsx
+++ src/app/public/[projectId]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { QRCodeCanvas } from "qrcode.react";
